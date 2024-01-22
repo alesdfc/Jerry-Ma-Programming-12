34333e2224c60978753e9cc7522133afb51869ce
Jerry Ma <alesdfcc@gmail.com>
2024-01-22 13:07:33 -0800

    wow!

--- Modules2/Module4/Main.java
+++ Modules2/Module4/Main.java
@@ -1,22 +1,20 @@
 package Module4;
 import java.io.*;
-import java.nio.charset.Charset;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 public class Main {
-    // Reads lines from document and adds it into an ArrayList. Then asks
-    // user for input and calls for searchWord method
+    /* Reads each individual word and replaces any variation
+    in capitalization or punctuation with an empty space then places it into a hashset
+     */
     public static void main(String[] args) throws Exception{
-        // Read lines from text file
-        HashSet<String> text = new HashSet<>();
-        Scanner input=new Scanner(new File("illiad.txt"));
-        input.useDelimiter(" +"); //delimitor is one or more spaces
-
-        while(input.hasNext()){
-            text.add(input.next());
+        Set<String> uniqueWords = new HashSet<String>();
+        Scanner input = new Scanner(new File("illiad.txt"));
+        while (input.hasNext()) {
+            String word = input.next().toLowerCase().replaceAll("[^*+a-z=$%&@0-9/]", "");
+            uniqueWords.add(word);
         }
-        System.out.println("There are " + text.size() + " unique words in this this text file");
+        input.close();
+        System.out.println("Number of unique words: " + uniqueWords.size());
     }
 }
